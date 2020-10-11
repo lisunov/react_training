be1d40e80f60e499e688fb40148579b22d85dfe3
Aleksei Lisunov <aleksei.lisunov@mera.com>
2020-10-12 00:17:13 +0300

    styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import AppHeader from '../app-header'
-import SearchPanel from '../search-panel'
-import PostStatusFilter from '../post-status-filter'
-import PostList from '../post-list'
-import PostAddForm from '../post-add-form'
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
 
-import './app.css'
+import './app.css';
+// import style from './App.module.css';
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+`;
+
+const StyledAppBlock = styled(AppBlock)`
+  background-color: grey;
+`;
 
 const App = () => {
 
@@ -16,7 +27,7 @@ const App = () => {
   ];
 
   return (
-    <div className="app">
+    <AppBlock>
       <AppHeader/>
       <div className="search-panel d-flex">
         <SearchPanel/>
@@ -24,7 +35,7 @@ const App = () => {
       </div>
       <PostList posts={data}/>
       <PostAddForm/>
-    </div>
+    </AppBlock>
     );
 };
 export default App;
